9ac674008c22f65443f8ca3b9499e646ef43089e
agali <agali.niko@gmail.com>
2022-07-05 22:16:57 +0400

    test 1

--- src/gun1/Ornek1.java
+++ src/gun1/Ornek1.java
@@ -1,5 +1,15 @@
 package gun1;
 
 public class Ornek1 {
-    
+    public static void main(String[] args) {
+        System.out.println("merhaba dunya");
+
+        System.out.println("merhaba 2");
+
+
+        System.out.println("merhaba 4");
+
+
+
+    }
 }
